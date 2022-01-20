56e031c913ff14fb83ecd5190146595e403babb6
Ethan Varghese <ethanvar6@gmail.com>
2022-01-20 17:34:32 -0500

    removed html file

--- public/orderform.js
+++ public/orderform.js
@@ -306,10 +306,7 @@ let restaurants = {"Aragorn's Orc BBQ": aragorn, "Lembas by Legolas": legolas, "
 
 //The drop-down menu
 let select = document.getElementById("restaurant-select");
-//Stores the currently selected restaurant index to allow it to be set back when switching restaurants is cancelled by user
 let currentSelectIndex = select.selectedIndex
-//Stores the current restaurant to easily retrieve data. The assumption is that this object is following the same format as the data included above. If you retrieve the restaurant data from the server and assign it to this variable, the client order form code should work automatically.
-//Stored the order data. Will have a key with each item ID that is in the order, with the associated value being the number of that item in the order.
 let order = {};
 
 
@@ -383,10 +380,8 @@ function getCategoryHTML(rest){
 function getMenuHTML(rest){
 	let menu = rest.menu;
 	let result = "";
-	//For each category in the menu
 	Object.keys(menu).forEach(key =>{
 		result += `<b>${key}</b><a name="${key}"></a><br>`;
-		//For each menu item in the category
 		Object.keys(menu[key]).forEach(id => {
 			item = menu[key][id];
 			result += `${item.name} (\$${item.price}) <img src='add.png' style='height:20px;vertical-align:bottom;' onclick='addItem(${id})'/> <br>`;
@@ -396,7 +391,6 @@ function getMenuHTML(rest){
 	return result;
 }
 
-//Responsible for adding one of the item with given id to the order and updating the summary
 function addItem(id){
 	if(order.hasOwnProperty(id)){
 		order[id].quantity += 1;
@@ -408,7 +402,7 @@ function addItem(id){
 	updateOrder();
 }
 
-//Responsible for removing one of the items with given id from the order and updating the summary
+
 function removeItem(id){
 	if(order.hasOwnProperty(id)){
 		order[id].quantity -= 1;
@@ -419,29 +413,23 @@ function removeItem(id){
 	}
 }
 
-//Reproduces new HTML containing the order summary and updates the page
-//This is called whenever an item is added/removed in the order
 function updateOrder(){
 	let result = "";
 	currentSubtotal = 0;
-	
-	//For each item ID currently in the order
+
 	Object.keys(order).forEach(id =>{
-		//Retrieve the item from the menu data using helper function
-		//Then update the subtotal and result HTML
 		let item = getItemById(id);
 		currentSubtotal += (item.price * order[id].quantity);
 		result += `${item.name} x ${order[id].quantity} (${(item.price * order[id].quantity).toFixed(2)}) <img src='remove.png' style='height:15px;vertical-align:bottom;' onclick='removeItem(${id})'/><br>`;
 	});
-	
-	//Add the summary fields to the result HTML, rounding to two decimal places
+
 	result += `Subtotal: \$${currentSubtotal.toFixed(2)}<br>`;
 	result += `Tax: \$${(currentSubtotal*0.1).toFixed(2)}<br>`;
 	result += `Delivery Fee: \$${currentRestaurant.delivery_fee.toFixed(2)}<br>`;
 	currentTotal = currentSubtotal + (currentSubtotal*0.1) + currentRestaurant.delivery_fee;
 	result += `Total: \$${currentTotal.toFixed(2)}<br>`;
 	
-	//Decide whether to show the Submit Order button or the Order X more label
+
 	if(currentSubtotal >= currentRestaurant.min_order){
 		result += `<button type="button" id="submit" onclick="submitOrder()">Submit Order</button>`
 	}else{
@@ -451,7 +439,6 @@ function updateOrder(){
 	document.getElementById("right").innerHTML = result;
 }
 
-//Helper function. Given an ID of an item in the current restaurant's menu, returns that item object if it exists.
 function getItemById(id){
 	let categories = Object.keys(currentRestaurant.menu);
 	for(let i = 0; i < categories.length; i++){
@@ -462,7 +449,6 @@ function getItemById(id){
 	return null;
 }
 
-//Helper function. Returns true if object is empty, false otherwise.
 function isEmpty(obj) {
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
